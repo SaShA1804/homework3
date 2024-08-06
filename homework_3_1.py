6af6675c366cd35f7ebd184796e33201d76b67da
SaShA1804 <oleksandrpincuk961@gmail.com>
2024-08-06 13:11:39 +0300

    Update homework_3_1.py

--- homework_3_1.py
+++ homework_3_1.py
@@ -3,24 +3,25 @@ from datetime import datetime
 def get_date_input():
     while True:
         try:
-            date_string = input("Будь ласка, введіть дату у форматі день-місяць-рік (наприклад,2024-06-26 ): ")
+            date_string = input("Будь ласка, введіть дату у форматі рік-місяць-день (наприклад, 2024-06-26): ")
             datetime.strptime(date_string, "%Y-%m-%d")
             return date_string
         except ValueError:
-            print("Дата введена неправильно, спробуйте ще раз")
-       
+            print("Дата введена неправильно, спробуйте ще раз.")
+
 def get_days_from_today(date):
-    # Преобразование строки даты в объект datetime
-    date_time_input = datetime.strptime(date, "%Y-%m-%d").date()
-    today = datetime.today().date()
-    # Вычисление разницы в днях
-    difference = date_time_input - today
-    return difference.days
+    try:
+        date_time_input = datetime.strptime(date, "%Y-%m-%d").date()
+        today = datetime.today().date()
+        difference = date_time_input - today
+        return difference.days
+    except ValueError:
+        print("Формат дати невірний. Будь ласка, введіть дату у форматі рік-місяць-день (наприклад, 2024-06-26).")
+        return None
 
-# Отримання дати від користувача
 date = get_date_input()
-# Обчислення різниці в днях
-difference_result = get_days_from_today(date)
-# Виведення результату
-print(f"Різниця між датами: {difference_result} днів")
 
+difference_result = get_days_from_today(date)
+
+if difference_result is not None:
+    print(f"Різниця між датами: {difference_result} днів")
